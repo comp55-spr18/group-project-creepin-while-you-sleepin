f731898615d418936a1d775080a874e266046f7d
River <astrofinity@gmail.com>
2018-03-24 22:14:39 -0700

    Added selectedGun attribute to ship class and added PlayerShip class

---
+++ src/PlayerShip.java
@@ -0,0 +1,39 @@
+import java.awt.Color;
+
+import acm.graphics.GImage;
+import acm.graphics.GPoint;
+
+public class PlayerShip extends Ship {
+	public PlayerShip() {
+		setInvincible(false);
+		setIframe(100);
+		setHealth(5);
+		setCooldown(0);
+		setMaxCooldown(1);
+		setCanShoot(true);
+		setShipPoints(new GPoint[] {});
+		setLocation(new GPoint(0,0));
+		setGunLocation(new GPoint[] {new GPoint(50,17.5)});
+		setSprite(new GImage("images/auto.png", getLocation().getX(), getLocation().getY()));
+		setBulletColor(Color.BLUE);
+		getSprite().setSize(50, 50);
+	}
+	@Override
+	public void move() {
+		double x = getLocation().getX();
+		double y = getLocation().getY();
+		setShipPoints(new GPoint[]{new GPoint(x,y), new GPoint(x,y+10), new GPoint(x,y+20), new GPoint(x,y+30), new GPoint(x,y+40), new GPoint(x,y+50), new GPoint(x+10,y+50), new GPoint(x+20,y+50), new GPoint(x+30,y+50), new GPoint(x+40,y+50), new GPoint(x+50,y+50),new GPoint(x+50,y+40), new GPoint(x+50,y+30), new GPoint(x+50,y+20), new GPoint(x+50,y+10), new GPoint(x+50, y), new GPoint(x+40, y), new GPoint(x+30, y), new GPoint(x+20, y), new GPoint(x+10, y)});
+		setGunLocation(new GPoint[] {new GPoint(x+50,y+17.5)});
+		getSprite().setLocation(getLocation());
+	}
+	@Override
+	public Projectile shoot() {
+		Projectile returnProj = new Projectile(true, getGunLocation()[getSelectedGun()], 1, 0, 14, getBulletColor(), 15);
+		if(getSelectedGun() == getGunLocation().length) {
+			setSelectedGun(0);
+		} else {
+			setSelectedGun(getSelectedGun() + 1);
+		}
+		return returnProj;
+	}
+}
--- src/Ship.java
+++ src/Ship.java
@@ -5,22 +5,23 @@ import acm.graphics.*;
 import acm.program.*;
 
 public class Ship {
-	public GPoint[] shipPoints;		// The points on the ship that will be checked for collision
-	public GImage sprite;			// The image that will be displayed for the ship
-	public GPoint[] gunLocation;	// The points that will be used to fire projectiles, if any 
-	public GPoint location;			// The top-left most point of the ship (like a GRectangle's location)
-	public Color bulletColor;		// The color of the projectiles fired by this ship
-	boolean canShoot;				// Trigger that toggles on/off on cooldown in the main loop
-	boolean invincible;				// Trigger that toggles on/off on cooldown in the main loop
-	int iframe;						// Number of frames that the ship will be invincible for if at all
-	int health;						// The number of hits the ship can take before being destroyed
-	int cooldown;					// The initial value of cooldown (Set to 0 if the ship can fire as soon as it spawns)
-	int maxCooldown;				// The number of frames between each call of the Shoot() function
+	private GPoint[] shipPoints;		// The points on the ship that will be checked for collision
+	private GImage sprite;				// The image that will be displayed for the ship
+	private GPoint[] gunLocation;		// The points that will be used to fire projectiles, if any
+	private int selectedGun;			// Integer switch that selects which point in gunLocation array to fire from
+	private GPoint location;			// The top-left most point of the ship (like a GRectangle's location)
+	private Color bulletColor;			// The color of the projectiles fired by this ship
+	private boolean canShoot;			// Trigger that toggles on/off on cooldown in the main loop
+	private boolean invincible;			// Trigger that toggles on/off on cooldown in the main loop
+	private int iframe;					// Number of frames that the ship will be invincible for if at all
+	private int health;					// The number of hits the ship can take before being destroyed
+	private int cooldown;				// The initial value of cooldown (Set to 0 if the ship can fire as soon as it spawns)
+	private int maxCooldown;			// The number of frames between each call of the Shoot() function
 	
 	// These attributes only apply to enemy ships
-	int xDir;						// The number of pixels the ship will move on the x-axis per frame
-	int yDir;						// The number of pixels the ship will move on the y-axis per frame
-	int speed;						// Multiplicative speed modifier to x/yDir
+	int xDir;			// Since each move() is different for each ship, these do whatever you make them do
+	int yDir;
+	int speed;
 
 	public void move() {
 		System.out.println("Needs to be accessed by child class");
@@ -28,5 +29,96 @@ public class Ship {
 	public Projectile shoot() {
 		System.out.println("Needs to be accessed by child class");
 		return null;
+	}
+	// Getters and setters, nothing important down here
+	public GPoint[] getShipPoints() {
+		return shipPoints;
+	}
+	public void setShipPoints(GPoint[] shipPoints) {
+		this.shipPoints = shipPoints;
+	}
+	public GImage getSprite() {
+		return sprite;
+	}
+	public void setSprite(GImage sprite) {
+		this.sprite = sprite;
+	}
+	public GPoint[] getGunLocation() {
+		return gunLocation;
+	}
+	public void setGunLocation(GPoint[] gunLocation) {
+		this.gunLocation = gunLocation;
+	}
+	public GPoint getLocation() {
+		return location;
+	}
+	public void setLocation(GPoint location) {
+		this.location = location;
+	}
+	public Color getBulletColor() {
+		return bulletColor;
+	}
+	public void setBulletColor(Color bulletColor) {
+		this.bulletColor = bulletColor;
+	}
+	public boolean isCanShoot() {
+		return canShoot;
+	}
+	public void setCanShoot(boolean canShoot) {
+		this.canShoot = canShoot;
+	}
+	public boolean isInvincible() {
+		return invincible;
+	}
+	public void setInvincible(boolean invincible) {
+		this.invincible = invincible;
+	}
+	public int getIframe() {
+		return iframe;
+	}
+	public void setIframe(int iframe) {
+		this.iframe = iframe;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public int getCooldown() {
+		return cooldown;
+	}
+	public void setCooldown(int cooldown) {
+		this.cooldown = cooldown;
+	}
+	public int getMaxCooldown() {
+		return maxCooldown;
+	}
+	public void setMaxCooldown(int maxCooldown) {
+		this.maxCooldown = maxCooldown;
+	}
+	public int getxDir() {
+		return xDir;
+	}
+	public void setxDir(int xDir) {
+		this.xDir = xDir;
+	}
+	public int getyDir() {
+		return yDir;
+	}
+	public void setyDir(int yDir) {
+		this.yDir = yDir;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getSelectedGun() {
+		return selectedGun;
+	}
+	public void setSelectedGun(int selectedGun) {
+		this.selectedGun = selectedGun;
 	}
 }
