564cad3e4b1c2f9d0e5861c7af61801b6e7b4cac
River <astrofinity@gmail.com>
2018-03-24 22:17:20 -0700

    Added comments to playership because I forgot

--- src/PlayerShip.java
+++ src/PlayerShip.java
@@ -19,7 +19,7 @@ public class PlayerShip extends Ship {
 		getSprite().setSize(50, 50);
 	}
 	@Override
-	public void move() {
+	public void move() {	// Moves the player's ship hitbox to the location of the ship
 		double x = getLocation().getX();
 		double y = getLocation().getY();
 		setShipPoints(new GPoint[]{new GPoint(x,y), new GPoint(x,y+10), new GPoint(x,y+20), new GPoint(x,y+30), new GPoint(x,y+40), new GPoint(x,y+50), new GPoint(x+10,y+50), new GPoint(x+20,y+50), new GPoint(x+30,y+50), new GPoint(x+40,y+50), new GPoint(x+50,y+50),new GPoint(x+50,y+40), new GPoint(x+50,y+30), new GPoint(x+50,y+20), new GPoint(x+50,y+10), new GPoint(x+50, y), new GPoint(x+40, y), new GPoint(x+30, y), new GPoint(x+20, y), new GPoint(x+10, y)});
@@ -27,7 +27,7 @@ public class PlayerShip extends Ship {
 		getSprite().setLocation(getLocation());
 	}
 	@Override
-	public Projectile shoot() {
+	public Projectile shoot() {		// Returns the projectile type and iterates to the next gun location (or the same one if only one)
 		Projectile returnProj = new Projectile(true, getGunLocation()[getSelectedGun()], 1, 0, 14, getBulletColor(), 15);
 		if(getSelectedGun() == getGunLocation().length) {
 			setSelectedGun(0);
