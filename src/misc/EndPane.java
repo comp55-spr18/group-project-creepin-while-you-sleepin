b660b10cc58e0f0dff034df8391b71f89bb29163
River <astrofinity@gmail.com>
2018-04-19 15:33:12 -0700

    Background uncommented back into the game for end pane

--- src/misc/EndPane.java
+++ src/misc/EndPane.java
@@ -1,6 +1,8 @@
 package misc;
 import java.awt.Color;
 import java.awt.event.MouseEvent;
+
+import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.graphics.GObject;
 import game.Game;
@@ -9,7 +11,7 @@ public class EndPane extends GraphicsPane {
 	private GButton rect;
 	private GButton rect2;
 	private GLabel endLabel;
-//	private GImage background;
+	private GImage background;
 
 	public EndPane(Game app) {
 		super();
@@ -22,13 +24,14 @@ public class EndPane extends GraphicsPane {
 		rect2.setFillColor(Color.RED);
 		endLabel = new GLabel("");
 		endLabel.setFont("Arial-Bold-100");
-//		background = new GImage("mainmenu.jpg");
-//		background.setSize(program.getWidth(), program.getHeight());
+		endLabel.setColor(Color.WHITE);
+		background = new GImage("levels/betweenbackground.jpg");
+		background.setSize(program.getWidth(), program.getHeight());
 	}
 
 	@Override
 	public void showContents() {
-//		program.add(background);
+		program.add(background);
 		if(program.lose) {
 			endLabel.setLabel("You lose!");
 		} else {
@@ -42,7 +45,7 @@ public class EndPane extends GraphicsPane {
 
 	@Override
 	public void hideContents() {
-//		program.remove(background);
+		program.remove(background);
 		program.remove(endLabel);
 		program.remove(rect);
 		program.remove(rect2);
